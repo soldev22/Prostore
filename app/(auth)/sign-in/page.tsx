07b15cac96c717bef43e347076889fb7691d3072
Soldev22 <mike@solutionsdeveloped.co.uk>
2025-03-27 13:59:45 +0000

    Bug fixes after 4

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
-
-import { auth } from '@/auth';
 import {
   Card,
   CardContent,
